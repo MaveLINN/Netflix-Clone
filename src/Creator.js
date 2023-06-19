1740d6770458e1024e1f914ae89250894a73bb68
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2023-06-19 19:26:49 +0530

    Update Creator.js
    portfolio url update

--- src/Creator.js
+++ src/Creator.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Creator() {
     return (
         <div >
-            <h3 style={{color: 'white',marginLeft:'540px'}}> Created by-Gaurav Singh ( <a href="https://mavelinn.github.io/">mavelinn.github.io</a> )</h3> 
+            <h3 style={{color: 'white',marginLeft:'540px'}}> Created by-Gaurav Singh ( <a href="https://mavelinn.github.io/portfolio/">mavelinn.github.io</a> )</h3> 
         </div>
     )
 }
